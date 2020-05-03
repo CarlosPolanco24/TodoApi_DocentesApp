260a8089b072eb4c450079951eabdee1f3bd3a3b
CarlosPolanco24 <42209540+CarlosPolanco24@users.noreply.github.com>
2020-05-02 22:39:38 -0500

    Recuperar Contraseña

---
+++ TodoApi/Controllers/RecuperarContrasenaController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TodoApi.Controllers
+{
+    [AllowAnonymous]
+    [RoutePrefix("api/recuperarcontrasena")]
+    public class RecuperarContrasenaController : ApiController
+    {
+        [HttpGet]
+        [Route("echoping")]
+        public IHttpActionResult EchoPing()
+        {
+            return Ok(true);
+        }
+
+        [HttpPost]
+        [Route("authenticate")]
+        public IHttpActionResult Authenticate(RecuperarContrasenaRequest recuperarContrasena)
+        {
+            if (recuperarContrasena.Email.Contains("@") && recuperarContrasena.Email.Contains(".com") || recuperarContrasena.Email.Contains("@") && recuperarContrasena.Email.Contains(".co"))
+            {
+                ResponseContrasena responseContrasena = new ResponseContrasena
+                {
+                    Ok = 1
+                };
+
+                return Ok(responseContrasena);
+            }
+            else
+            {
+                ResponseContrasena responseContrasena = new ResponseContrasena
+                {
+                    Ok = 0
+                };
+
+                return Ok(responseContrasena);
+            }
+
+            //if (recuperarContrasena.Email == "carlos.polanco.0ds@utap.edu.co")
+            //{
+            //    ResponseDTO responseDTO = new ResponseDTO
+            //    {
+            //        Exitoso = 1
+            //    };
+
+            //    return Ok(responseDTO);
+            //}
+            //else
+            //{
+            //    ResponseDTO responseDTO = new ResponseDTO
+            //    {
+            //        Exitoso = 0
+            //    };
+
+            //    return Ok(responseDTO);
+            //}
+        }
+    }
+}
---
+++ TodoApi/Models/RecuperarContrasenaRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TodoApi
+{
+    public class RecuperarContrasenaRequest
+    {
+        public string Email { get; set; }
+        public bool Button { get; set; }
+    }
+}
